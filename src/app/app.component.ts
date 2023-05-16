0f30f3d10b91bbbdbb5b2e39aa2b6cd2d83a6681
Simone Lopes <sisilopes20@gmail.com>
2023-05-16 13:08:24 +0100

    Adicionando items no localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,14 +19,15 @@ export class AppComponent {
         Validators.maxLength(60),
         Validators.required
       ])]
-    })
+    });
+    this.load();
   }
 
   add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false))
-
+    this.save();
     this.clear();
   }
 
@@ -35,18 +36,30 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
   clear() {
     this.form.reset();
   }
 
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data || '{}');
+  }
 }
